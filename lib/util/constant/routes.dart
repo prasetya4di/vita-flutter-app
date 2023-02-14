5f8ccc071c3e9925e2723389b4c7d546cdebdb93
Pras <prasetyaarfanto@gmail.com>
2023-02-14 13:59:23 +0700

    Add routes constant

---
+++ lib/util/constant/routes.dart
@@ -0,0 +1,7 @@
+class Routes {
+  static const String splashScreen = "splash";
+  static const String homeScreen = "home";
+  static const String login = "login";
+  static const String register = "register";
+  static const String profile = "profile";
+}
