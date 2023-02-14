046594c39f10acce97344235b64516aa3e7becdd
Prasetya Adi Arfanto <84487845+prasetya4di@users.noreply.github.com>
2023-02-14 22:02:15 +0700

    Update message dao

--- lib/data/source/local/impl/message_dao_impl.dart
+++ lib/data/source/local/impl/message_dao_impl.dart
@@ -1,13 +1,19 @@
 import 'package:vita_client_app/data/model/entity/message.dart';
 import 'package:vita_client_app/data/source/local/message_dao.dart';
+import 'package:vita_client_app/objectbox.g.dart';
 
 class MessageDaoImpl implements MessageDao {
+  Box<Message> _boxMessage;
+
+  MessageDaoImpl(this._boxMessage);
 
   @override
-  inserts(List<Message> messages) async {}
+  inserts(List<Message> messages) async {
+    _boxMessage.putMany(messages);
+  }
 
   @override
   Future<List<Message>> get() async {
-    return [];
+    return _boxMessage.getAll();
   }
 }
