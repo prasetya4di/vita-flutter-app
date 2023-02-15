d593f35b5d3a42c1643d7bff99f281e67e6a5cbf
Prasetya Adi Arfanto <84487845+prasetya4di@users.noreply.github.com>
2023-02-15 14:46:00 +0700

    Add di

---
+++ lib/core/di.dart
@@ -0,0 +1,40 @@
+import 'package:get_it/get_it.dart';
+import 'package:objectbox/objectbox.dart';
+import 'package:vita_client_app/data/model/entity/message.dart';
+import 'package:vita_client_app/data/source/local/impl/message_dao_impl.dart';
+import 'package:vita_client_app/data/source/local/message_dao.dart';
+import 'package:vita_client_app/data/source/local/objectbox.dart';
+import 'package:vita_client_app/data/source/network/chopper_service.dart';
+import 'package:vita_client_app/data/source/network/image_service.dart';
+import 'package:vita_client_app/data/source/network/message_service.dart';
+import 'package:vita_client_app/domain/fetch_message.dart';
+import 'package:vita_client_app/domain/impl/fetch_message_impl.dart';
+import 'package:vita_client_app/domain/impl/read_message_impl.dart';
+import 'package:vita_client_app/domain/impl/send_message_impl.dart';
+import 'package:vita_client_app/domain/read_message.dart';
+import 'package:vita_client_app/domain/send_message.dart';
+import 'package:vita_client_app/repository/impl/message_repository_impl.dart';
+import 'package:vita_client_app/repository/message_repository.dart';
+
+final di = GetIt.I;
+
+Future<void> setupServiceLocator() async {
+  // dao
+  final objectBox = await ObjectBox.create();
+  di.registerSingleton<Box<Message>>(objectBox.store.box<Message>());
+  di.registerSingleton<MessageDao>(MessageDaoImpl(di.get()));
+
+  // service
+  di.registerSingleton<MessageService>(
+      chopperClient.getService<MessageService>());
+  di.registerSingleton<ImageService>(chopperClient.getService<ImageService>());
+
+  // repository
+  di.registerSingleton<MessageRepository>(
+      MessageRepositoryImpl(di.get(), di.get()));
+
+  // use case
+  di.registerSingleton<FetchMessage>(FetchMessageImpl(di.get()));
+  di.registerSingleton<ReadMessage>(ReadMessageImpl(di.get()));
+  di.registerSingleton<SendMessage>(SendMessageImpl(di.get()));
+}
