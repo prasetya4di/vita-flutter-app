4656da90511498599848e9fd876285b2d5635687
Pras <prasetyaarfanto@gmail.com>
2023-02-15 08:13:36 +0700

    Add json type parser

---
+++ lib/util/converter/parser_exception.dart
@@ -0,0 +1,11 @@
+class PathNotFoundException implements Exception {
+  final String message;
+
+  PathNotFoundException([this.message = '']);
+}
+
+class JsonFactoryNotFoundException implements Exception {
+  final String message;
+
+  JsonFactoryNotFoundException([this.message = '']);
+}
