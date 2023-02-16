36b4ae6e3947d415e62d19bdf69fc574f6574637
Pras <prasetyaarfanto@gmail.com>
2023-02-16 11:23:35 +0700

    update chat screen

--- lib/view/chat/chat_screen.dart
+++ lib/view/chat/chat_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:vita_client_app/view/chat/widget/chat_text_field.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -7,31 +7,14 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Vita", style: Theme.of(context).textTheme.titleMedium),
-      ),
-      bottomNavigationBar: Container(
-        padding: const EdgeInsets.symmetric(vertical: 28, horizontal: 18),
-        child: Row(
-          children: [
-            Container(
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(8)),
-                color: Colors.white60,
-              ),
-              width: 265,
-              child: TextFormField(
-                textAlignVertical: TextAlignVertical.center,
-                style: Theme.of(context).textTheme.bodySmall,
-                decoration: const InputDecoration(fillColor: Colors.grey),
-              ),
-            ),
-            TextButton(
-                onPressed: () {},
-                child: Text(AppLocalizations.of(context).sendButton))
-          ],
+        appBar: AppBar(
+          title: Text("Vita", style: Theme.of(context).textTheme.titleMedium),
         ),
-      ),
-    );
+        body: Column(
+          children: const [
+            Expanded(child: Text("Chat message")),
+            ChatTextField()
+          ],
+        ));
   }
 }
