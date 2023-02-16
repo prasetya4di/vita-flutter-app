87931235d9c10c6683fe2d7ccda6e75c82d00e44
Pras <prasetyaarfanto@gmail.com>
2023-02-16 10:51:16 +0700

    Add color.dart

---
+++ lib/util/extension/color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+extension on Colors {
+  static const gray50 = Color(0xFFEFEFEF);
+  static const gray100 = Color(0xFFF3F3F3);
+  static const gray200 = Color(0xFFBDBDBD);
+  static const gray300 = Color(0xFFAEAEAE);
+  static const green100 = Color(0xFF2FCC59);
+  static const blue100 = Color(0xFF278EFF);
+}
