d58fc0a1fc1f5e57b12653523f383dd2507b844a
Pras <prasetyaarfanto@gmail.com>
2023-02-16 11:59:48 +0700

    Add profile icon

--- lib/view/chat/chat_screen.dart
+++ lib/view/chat/chat_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:vita_client_app/generated/assets.dart';
+import 'package:vita_client_app/util/extension/color_extension.dart';
 import 'package:vita_client_app/view/chat/widget/chat_reply.dart';
 import 'package:vita_client_app/view/chat/widget/chat_send.dart';
 import 'package:vita_client_app/view/chat/widget/chat_text_field.dart';
@@ -11,6 +14,15 @@ class ChatScreen extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: Text("Vita", style: Theme.of(context).textTheme.titleMedium),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  //Todo add function navigate to profile
+                },
+                icon: SvgPicture.asset(Assets.imagesIcUser))
+          ],
+          backgroundColor: AssetColor.gray50,
+          shadowColor: Colors.black.withOpacity(0.2),
         ),
         body: Column(
           children: const [
