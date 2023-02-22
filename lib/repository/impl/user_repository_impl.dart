27547abc2fd9b3ce29532b6f1b9ee186d1cec95c
Pras <prasetyaarfanto@gmail.com>
2023-02-22 09:57:46 +0700

    Add user repository

---
+++ lib/repository/impl/user_repository_impl.dart
@@ -0,0 +1,41 @@
+import 'package:chopper/src/response.dart';
+import 'package:vita_client_app/data/model/entity/user.dart';
+import 'package:vita_client_app/data/model/request/login_request.dart';
+import 'package:vita_client_app/data/model/request/register_request.dart';
+import 'package:vita_client_app/data/model/response/login_response.dart';
+import 'package:vita_client_app/data/model/response/register_response.dart';
+import 'package:vita_client_app/data/source/local/user_dao.dart';
+import 'package:vita_client_app/data/source/network/user_service.dart';
+import 'package:vita_client_app/repository/user_repository.dart';
+
+class UserRepositoryImpl implements UserRepository {
+  final UserDao _userDao;
+  final UserService _userService;
+
+  UserRepositoryImpl(this._userDao, this._userService);
+
+  @override
+  String getToken() {
+    return _userDao.getToken();
+  }
+
+  @override
+  insert(User user) {
+    _userDao.insert(user);
+  }
+
+  @override
+  Future<Response<LoginResponse>> login(LoginRequest request) async {
+    return _userService.login(request);
+  }
+
+  @override
+  User read() {
+    return _userDao.read();
+  }
+
+  @override
+  Future<Response<RegisterResponse>> register(RegisterRequest request) async {
+    return _userService.register(request);
+  }
+}
---
+++ lib/repository/user_repository.dart
@@ -0,0 +1,18 @@
+import 'package:chopper/chopper.dart';
+import 'package:vita_client_app/data/model/entity/user.dart';
+import 'package:vita_client_app/data/model/request/login_request.dart';
+import 'package:vita_client_app/data/model/request/register_request.dart';
+import 'package:vita_client_app/data/model/response/login_response.dart';
+import 'package:vita_client_app/data/model/response/register_response.dart';
+
+abstract class UserRepository {
+  insert(User user);
+
+  User read();
+
+  String getToken();
+
+  Future<Response<LoginResponse>> login(LoginRequest request);
+
+  Future<Response<RegisterResponse>> register(RegisterRequest request);
+}
