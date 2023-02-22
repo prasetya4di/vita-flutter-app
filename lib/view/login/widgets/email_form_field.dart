2c190059b179be4ffda05b5f005b7d9b93ed1c8f
Prasetya Adi Arfanto <84487845+prasetya4di@users.noreply.github.com>
2023-02-22 13:46:43 +0700

    Add email form field

---
+++ lib/view/login/widgets/email_form_field.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:vita_client_app/view/widgets/space_vertical.dart';
+
+class EmailFormField extends StatefulWidget {
+  const EmailFormField({super.key});
+
+  @override
+  State<EmailFormField> createState() => _EmailFormField();
+}
+
+class _EmailFormField extends State<EmailFormField> {
+  var inputBorder = const OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(8)),
+      borderSide: BorderSide(color: Colors.black));
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("Email", style: Theme.of(context).textTheme.labelMedium),
+        const SpaceVertical(),
+        TextFormField(
+          keyboardType: TextInputType.emailAddress,
+          style: Theme.of(context).textTheme.bodyMedium,
+          decoration: InputDecoration(
+            contentPadding:
+                const EdgeInsets.symmetric(vertical: 16, horizontal: 18),
+            filled: true,
+            border: inputBorder,
+            enabledBorder: inputBorder,
+          ),
+          autovalidateMode: AutovalidateMode.always,
+          validator: _validateEmail,
+        )
+      ],
+    );
+  }
+
+  String? _validateEmail(String? value) {
+    const pattern = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'"
+        r'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-'
+        r'\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*'
+        r'[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4]'
+        r'[0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9]'
+        r'[0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\'
+        r'x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])';
+    final regex = RegExp(pattern);
+
+    return value!.isNotEmpty && !regex.hasMatch(value)
+        ? 'Enter a valid email address'
+        : null;
+  }
+}
