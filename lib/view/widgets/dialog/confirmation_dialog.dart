a79ec79868f1520f8aa6163a0843ea008f773723
Pras <prasetyaarfanto@gmail.com>
2023-02-28 10:52:44 +0700

    Add confirmation dialog

---
+++ lib/view/widgets/dialog/confirmation_dialog.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:vita_client_app/view/widgets/dialog/base_dialog.dart';
+import 'package:vita_client_app/view/widgets/space_horizontal.dart';
+import 'package:vita_client_app/view/widgets/space_vertical.dart';
+
+Future<bool> showConfirmationDialog(
+    BuildContext context, String message) async {
+  return await showDialog(
+      context: context,
+      builder: (context) {
+        return BaseDialog(
+            onCloseTap: () {
+              Navigator.pop(context, false);
+            },
+            title: AppLocalizations.of(context).textConfirmation,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(message),
+                const SpaceVertical(size: 16),
+                Row(
+                  children: [
+                    Expanded(
+                      child: OutlinedButton(
+                        onPressed: () {
+                          Navigator.pop(context, false);
+                        },
+                        child: Text(AppLocalizations.of(context).textNo),
+                      ),
+                    ),
+                    const SpaceHorizontal(),
+                    Expanded(
+                      child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.pop(context, true);
+                          },
+                          child: Text(AppLocalizations.of(context).textYes)),
+                    )
+                  ],
+                )
+              ],
+            ));
+      });
+}
