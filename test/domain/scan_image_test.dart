44ea816d5658836ba0f2dbf72dab06105c3a5e1e
Pras <prasetyaarfanto@gmail.com>
2023-03-10 14:31:08 +0700

    Update scan image test

--- test/domain/scan_image_test.dart
+++ test/domain/scan_image_test.dart
@@ -6,6 +6,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:vita_client_app/data/model/response/scanned_image.dart';
 import 'package:vita_client_app/domain/impl/scan_image_impl.dart';
 import 'package:vita_client_app/domain/scan_image.dart';
@@ -31,11 +32,37 @@ void main() {
     XFile expectedFile = createXFile();
     ScannedImage expectedScannedImage = createScannedImageResponse();
 
-    test("Scan image success", () {
+    test("Scan image success", () async {
       Response<ScannedImage> expectedResponse = Response(
           http.Response(
               jsonEncode(expectedScannedImage.toJson()), HttpStatus.created),
           expectedScannedImage);
+
+      when(mockImageRepository.clear()).thenReturn(() {});
+      when(mockImageRepository.inserts(expectedScannedImage.possibilities))
+          .thenReturn(() {});
+      when(mockImageRepository.scanImage(expectedFile))
+          .thenAnswer((_) => Future.value(expectedResponse));
+      when(mockMessageRepository.inserts(expectedScannedImage.messages))
+          .thenReturn(() {});
+
+      var response = await scanImage.call(expectedFile);
+
+      verify(mockImageRepository.clear());
+      verify(mockImageRepository.inserts(expectedScannedImage.possibilities));
+      verify(mockImageRepository.scanImage(expectedFile));
+      verify(mockMessageRepository.inserts(expectedScannedImage.messages));
+
+      expect(response, expectedScannedImage);
+    });
+
+    test("Scan image error should throw an error", () {
+      HttpException expectedError = const HttpException("Invalid url");
+      when(mockImageRepository.scanImage(expectedFile))
+          .thenThrow(expectedError);
+      verifyNever(mockImageRepository.scanImage(expectedFile));
+      expect(() => scanImage.call(expectedFile),
+          throwsA(const TypeMatcher<HttpException>()));
     });
   });
 }
