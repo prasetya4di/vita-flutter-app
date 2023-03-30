d6ea5c116636a0dabebd05a9eb7bb31b93577a03
Pras <prasetyaarfanto@gmail.com>
2023-03-30 13:09:17 +0700

    Update register state

--- lib/view/register/bloc/register_state.dart
+++ lib/view/register/bloc/register_state.dart
@@ -4,13 +4,13 @@ import 'package:vita_client_app/data/model/request/register_request.dart';
 part 'register_state.freezed.dart';
 
 @freezed
-abstract class RegisterEvent with _$RegisterEvent {
+class RegisterEvent with _$RegisterEvent {
   const factory RegisterEvent.onRegister(RegisterRequest request) =
       PostRegisterEvent;
 }
 
 @freezed
-abstract class RegisterState with _$RegisterState {
+class RegisterState with _$RegisterState {
   const factory RegisterState.initial() = RegisterInitialState;
 
   const factory RegisterState.loading() = RegisterLoadingState;
